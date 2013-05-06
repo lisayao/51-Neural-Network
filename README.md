03501cb1316331cbdeafa5e43a41d502053d888b
Lisa Yao <lisacyao@gmail.com>
2013-05-06 17:40:48 -0400

    51 Submission

--- README.md
+++
@@ -1,4 +0,0 @@
-51-Neural-Network
-=================
-
-CS51 Final Project
--- readcsv.py
+++
@@ -1,8 +0,0 @@
-### Read in CSV file and make it into a vector
-
-import csv
-
-with open('train.csv', 'rb') as csvfile:
-	reader = csv.reader(csvfile, delimiter=' ')
-	for row in reader:
-		vector = row
